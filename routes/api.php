049e9fe524d5673ec20161d418cceb7ab7b06a9d
UNIsamuel3103 <Samuel.penharrieta@gmail.com>
2025-12-08 00:32:12 -0400

    correcion de formatos

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\Api\InternalNoteController;
 use App\Http\Controllers\Api\MacroController;
+use App\Http\Controllers\InternalNoteController as InternalNoteWebController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', 'ext.auth'])->group(function () {
     Route::get('internal-notes/{ticket}', [InternalNoteController::class, 'index'])
         ->name('api.internal-notes.index');
+    Route::post('internal-notes', [InternalNoteWebController::class, 'store'])
+        ->name('api.internal-notes.store');
     Route::get('macros', [MacroController::class, 'index'])
         ->name('api.macros.index');
 });
